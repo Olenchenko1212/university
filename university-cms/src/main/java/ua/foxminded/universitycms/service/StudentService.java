a7984b125c50e958a9489fb59e0807e985fcb6a9
qscaz <qscaz@host.docker.internal>
2023-11-07 12:05:22 +0200

    Changes 3:
    - remove @Service in interface StudentService

--- university-cms/src/main/java/ua/foxminded/universitycms/service/StudentService.java
+++ university-cms/src/main/java/ua/foxminded/universitycms/service/StudentService.java
@@ -4,11 +4,8 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.stereotype.Service;
-
 import ua.foxminded.universitycms.models.Student;
 
-@Service
 public interface StudentService {
 	List<Student> getAllStudents() throws SQLException;
 	Optional<Student> getStudentById(Long studentId) throws SQLException;
